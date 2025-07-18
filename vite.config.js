27ad2d96ce56c082d79f4a74b9e35cb97ccd09e7
1ethon0822 <1ethyjj9240@gmail.com>
2025-07-18 10:47:16 +0900

    vite build 설정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  build: {
+    outDir:
+      'D:/student/jyj/project1/rainbow-academic-be/src/main/resources/static',
+    emptyOutDir: true,
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
